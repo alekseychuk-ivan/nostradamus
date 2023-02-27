33f90135f48278013b96b5b5fec173ad13e9def4
alekseychuk-ivan <alekseychuk.ivan@gmail.ru>
2023-02-27 23:55:15 +0300

    added check response

--- parser_arteks.py
+++ parser_arteks.py
@@ -17,57 +17,60 @@ pagenurl = 'https://www.arteks.ooo/catalog/?PAGEN_1='
 
 #read catalog page
 result = requests.get(url=url, verify=False, headers=headers)
-soup = BeautifulSoup(result.text, 'lxml')
-
-# find count of page
-cnt_page = int(soup.find_all('a', class_='pagination__pages-link')[-1].get_text('\n', strip=True))
-catalog_dct = json.loads("{}")
-image_dct = []
-
-# read all page
-for page in range(1, cnt_page + 1):
-    print(f'{page} страница каталога из {cnt_page} страниц.')
-    result = requests.get(url=f'{pagenurl}{page}', verify=False)
+if result.status_code == 200:
     soup = BeautifulSoup(result.text, 'lxml')
 
-    # find all collection and download images for collection
-    all_products = soup.find_all('a', class_='catalog-item')
-    print(f'Найдено {len(all_products)} коллекций.')
-    for product in all_products:
-        collection = product.find(class_='catalog-item__collection').get_text('\n', strip=True)
-        collection = re.sub(r'[\n\s]', '_', collection)
-        print(f'Загружается коллекция {collection}.')
+    # find count of page
+    cnt_page = int(soup.find_all('a', class_='pagination__pages-link')[-1].get_text('\n', strip=True))
+    catalog_dct = json.loads("{}")
+    image_dct = []
 
-        # request page of product
-        # create new link
-        link = 'https://www.arteks.ooo' + product.get('href')
-        result = requests.get(link, verify=False, headers=headers)
+    # read all page
+    for page in range(1, cnt_page + 1):
+        print(f'{page} страница каталога из {cnt_page} страниц.')
+        result = requests.get(url=f'{pagenurl}{page}', verify=False)
         soup = BeautifulSoup(result.text, 'lxml')
 
-        # find section <script> var productObject </script> and get data
-        all_image = soup.find_all(['script', ], )[-1].get_text('\n', strip=True).split('=')[-1]
-        temp_json = json.loads(all_image[:-1])
-        catalog_dct.update(temp_json)
+        # find all collection and download images for collection
+        all_products = soup.find_all('a', class_='catalog-item')
+        print(f'Найдено {len(all_products)} коллекций.')
+        for product in all_products:
+            collection = product.find(class_='catalog-item__collection').get_text('\n', strip=True)
+            collection = re.sub(r'[\n\s]', '_', collection)
+            print(f'Загружается коллекция {collection}.')
 
-        #get data from json
-        for model in temp_json:
-            mod = temp_json[model]
-            textures = mod['textures']
+            # request page of product
+            # create new link
+            link = 'https://www.arteks.ooo' + product.get('href')
+            result = requests.get(link, verify=False, headers=headers)
+            soup = BeautifulSoup(result.text, 'lxml')
 
-            # download and save images
-            for texture in textures:
-                path = texture['mini']
-                name = path.split('/')[-1]
-                image_dct.append({name: model})
-                imgurl = 'https://www.arteks.ooo' + path
-                res = requests.get(url=imgurl, headers=headers, verify=False)
-                content = res.content
-                with open(Path(f'data/images/{collection}_{name}'), 'wb') as file:
-                    file.write(content)
-            break
+            # find section <script> var productObject </script> and get data
+            all_image = soup.find_all(['script', ], )[-1].get_text('\n', strip=True).split('=')[-1]
+            temp_json = json.loads(all_image[:-1])
+            catalog_dct.update(temp_json)
 
-with open(Path('data/catalog_arteks.json'), 'w', encoding='utf-8') as file:
-    json.dump(catalog_dct, file, indent=4, ensure_ascii=False)
+            #get data from json
+            for model in temp_json:
+                mod = temp_json[model]
+                textures = mod['textures']
 
-with open(Path('data/catalog.json'), 'w', encoding='utf-8') as file:
-    json.dump(image_dct, file, indent=4, ensure_ascii=False)
+                # download and save images
+                for texture in textures:
+                    path = texture['mini']
+                    name = path.split('/')[-1]
+                    image_dct.append({name: (model, collection)})
+                    imgurl = 'https://www.arteks.ooo' + path
+                    res = requests.get(url=imgurl, headers=headers, verify=False)
+                    content = res.content
+                    with open(Path(f'data/images/{name}'), 'wb') as file:
+                        file.write(content)
+                break
+
+    with open(Path('data/catalog_arteks.json'), 'w', encoding='utf-8') as file:
+        json.dump(catalog_dct, file, indent=4, ensure_ascii=False)
+
+    with open(Path('data/catalog.json'), 'w', encoding='utf-8') as file:
+        json.dump(image_dct, file, indent=4, ensure_ascii=False)
+else:
+    print(f'Error {result.status_code}')
