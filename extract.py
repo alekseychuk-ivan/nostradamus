b63398be5e3d3a1fdce8e31700e4f974c6046b9f
Alekseychuk_IA <Alekseychuk_IA@atomtex.com>
2023-04-04 16:42:13 +0300

    update

--- extract.py
+++ extract.py
@@ -11,19 +11,29 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
     # generate vectors for all the images in the set
     img2vec = Img2VecResnet18()
-
-    with open(Path('catalog/catalog.json'), 'r', encoding='utf-8') as file:
-        data = json.load(file)
-
+    allCatalog = []
     allVectors = []
-    print("Converting images to feature vectors:")
-    for d in tqdm.tqdm(data):
-        for image in d:
-            I = Image.open(os.path.join('data/images', image))
-            vec = img2vec.getVec(I)
-            allVectors.append(vec)
-            I.close()
+    files = ['catalog.json', 'allvectors.pt']
 
-    tensor = torch.stack(allVectors).to(device)
+    for file in files:
+        if os.path.exists(Path(f'catalog/{file}')):
+            os.remove(Path(f'catalog/{file}'))
+
+    for catalog in os.listdir('catalog'):
+        with open(Path(f'catalog/{catalog}'), 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+        print(f"Converting images from {catalog.split('.')[0]} to feature vectors:")
+        for d in tqdm.tqdm(data):
+            for image in d:
+                I = Image.open(os.path.join('data/images', image))
+                vec = img2vec.getVec(I)
+                allVectors.append(vec)
+                allCatalog.append(d)
+                I.close()
+
+        tensor = torch.stack(allVectors).to(device)
     with open(Path('catalog/allvectors.pt'), 'wb', ) as file:
         torch.save(tensor.to('cpu'), file)
+    with open(Path('catalog/catalog.json'), 'w', encoding='utf-8') as file:
+        json.dump(allCatalog, file, indent=4, ensure_ascii=False)
